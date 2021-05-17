e6ade08ac784211d5e89aa2efcee50b191092db8
R_H <hazarika.rr@gmail.com>
2021-05-17 18:26:36 +0200

    updated replicate.consensus function

--- filterDMRmatrix.R
+++ filterDMRmatrix.R
@@ -1,8 +1,6 @@
 
 filterReplicateConsensus <- function(status.collect, rc.methlevel.collect, replicate.consensus, gap=1){
   
-  pb1 <- txtProgressBar(min = 1, max = NROW(status.collect), char = "=", style = 3, file = "")
-  
   if (!is.null(status.collect$epiMAF)){
     status.collect <- status.collect[,-c("epiMAF")]
   }
@@ -12,28 +10,32 @@ filterReplicateConsensus <- function(status.collect, rc.methlevel.collect, repli
   colnames(sampleinfo) <- c("sample", "replicate")
   
   dt <- data.frame()
+  pb1 <- txtProgressBar(min = 1, max = NROW(status.collect), char = "=", style = 3, file = "")
+  
   q <- lapply(1:NROW(status.collect), function(x){
     mypattern <- unlist(status.collect[x, 4:NCOL(status.collect)])
     df.bind <- cbind(sampleinfo, mypattern)
     for (m in unique(df.bind$sample)){
-      rval <- replicate.consensus * length(df.bind$mypattern[df.bind$sample==m])
+      rval <- round(replicate.consensus * length(df.bind$mypattern[df.bind$sample==m]))
       pattern.vals <- df.bind$mypattern[df.bind$sample==m]
       tt <- table(pattern.vals)
-      if (max(tt) >= rval)
+      if (max(tt) >= rval){
         df.bind$count[df.bind$sample==m] <- 0
-      else
+      } else {
         df.bind$count[df.bind$sample==m] <- 1
-      
-      Sys.sleep(1/NROW(status.collect))
-      setTxtProgressBar(pb1, x)
+      }
     }
+    Sys.sleep(1/NROW(status.collect))
+    setTxtProgressBar(pb1, x)
     close(pb1)
     #print(df.bind)
-    if (sum(df.bind$count)==0)
+    if (sum(df.bind$count)==0) {
       dt <- rbind(dt, status.collect[x,])
+    }
   })
-  status.collect <- q[-which(sapply(q, is.null))]
-  df.status.collect <- rbindlist(status.collect)
+  out <- q[!sapply(q,is.null)]
+  #status.collect <- q[-which(sapply(q, is.null))]
+  df.status.collect <- rbindlist(out)
   if (NROW(df.status.collect) !=0){
   df.rc.methlevel.collect <- rc.methlevel.collect %>% semi_join(df.status.collect, by=c("seqnames","start","end"))
   return(list(df.status.collect, df.rc.methlevel.collect))
